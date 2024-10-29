96c93930ee68c328638c5882ac2c1cede5bdf284
0=2 dev <104338021+youngeunkim373@users.noreply.github.com>
2024-10-29 13:42:27 +0900

    feat: [PA-1] Initial setup (#5)
    * feat: [PA-3] set up eslint rules (#2)
    
    * feat: [PA-3] set up eslint
    
    * trivial: [PA-3] fix every file addording to eslint rules
    
    * feat: [PA-2] Install storybook and chromatic (#3)
    
    * feat: [PA-2] install storybook and set up to avoid conflicts with tailwind
    
    * feat: [PA-2] install chromatic
    
    * feat: [PA-2] publish chromatic to test chromatic
    
    * feat: [PA-5] Install and set up Stylex (#4)
    
    * feat: [PA-5] install and set up stylex
    
    * trivial: [PA-5] test stylex on index page to prove that it works properly
    
    * trivial: [PA-1] always import absolute paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,96 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  safelist: [
+    {
+      pattern: /bg-./,
+    },
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    colors: {
+      background: 'var(--background)',
+      foreground: 'var(--foreground)',
+
+      /* ---------- Custom theme colors----------- */ 
+      white: '#ffffff',
+      black: '#000000',
+      transparent: 'transparent',
+      gray: {
+        '50': '#f8fafc',
+        '100': '#f1f5f9',
+        '200': '#e2e8f0',
+        '300': '#cbd5e1',
+        '400': '#94a3b8',
+        '500': '#64748b',
+        '600': '#475569',
+        '700': '#334155',
+        '800': '#1e293b',
+        '900': '#0f172a',
+        '950': '#020617',
+      },
+      main: {
+        pale: '#ebe1ff',
+        light: '#c7abff',
+        DEFAULT: '#7a37ff',
+        dark: '#6428dd',
+      },
+      red: {
+        pale: '#fff1f2',
+        light: '#fda4af',
+        DEFAULT: '#f43f5e',
+        dark: '#e11d48',
+      },
+      blue: {
+        pale: '#eff6ff',
+        light: '#93c5fd',
+        DEFAULT: '#3b82f6',
+        dark: '#2563eb',
+      },
+      orange: {
+        pale: '#fff7ed',
+        light: '#fed7aa',
+        DEFAULT: '#fb923c',
+        dark: '#f97316',
       },
     },
+    extend: {}
   },
   plugins: [],
 };
 export default config;
+
+export type Colors = 
+  'white' | 
+  'black' | 
+  'transparent' |
+  'main' | 
+  'main-pale' | 
+  'main-light' | 
+  'main-dark' | 
+  'gray-50'| 
+  'gray-100'| 
+  'gray-200'| 
+  'gray-300'|
+  'gray-400' | 
+  'gray-500'| 
+  'gray-600'| 
+  'gray-700'| 
+  'gray-800'| 
+  'gray-900'| 
+  'gray-950' | 
+  'red' | 
+  'red-pale' | 
+  'red-light' | 
+  'red-dark' | 
+  'orange' | 
+  'orange-pale' | 
+  'orange-light' | 
+  'orange-dark' | 
+  'blue' | 
+  'blue-pale' | 
+  'blue-light' | 
+  'blue-dark';
