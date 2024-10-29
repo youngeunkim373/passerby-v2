e71d4b11fef8b55aa9ff3a7bf0842eef2b7efe87
zeroequaltwo <youngeunkim373@gmail.com>
2024-10-29 15:49:13 +0900

    feat: [PA-7] write sb doc of Button component

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryFn } from '@storybook/react';
+
+import { Button, Props as ButtonProps } from '@/components/Button';
+
+export default {
+  component: Button,
+  title: 'Components/Button/Button',
+  argTypes: {
+    className: { control: false }
+  }
+} as Meta<typeof Button>;
+
+const Template: StoryFn<ButtonProps> = ({ children, ...rest }) => {
+  return <Button {...rest}>{children}</Button>;
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+  color: 'main',
+  size: 'default',
+  variant: 'outlined',
+  children: 'Button',
+};
