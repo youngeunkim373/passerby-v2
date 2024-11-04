754cc3bf086dbdcde3f95d04c58572738f6c846b
zeroequaltwo <youngeunkim373@gmail.com>
2024-11-04 16:46:38 +0900

    trivial: [PA-11] rewirte Drawer sb doc according to changes

--- .storybook/Global.tsx
+++ .storybook/Global.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { DrawerContextProvider } from '../src/contexts/DrawerContext';
 import { ModalContextProvider } from '../src/contexts/ModalContext';
 
 export function Global(Story) {
   return (
-    <ModalContextProvider>
-      <Story />
-    </ModalContextProvider>
+    <DrawerContextProvider>
+      <ModalContextProvider>
+        <Story />
+      </ModalContextProvider>
+    </DrawerContextProvider>
   )
 };
 
