3ac5a9e78d5a507fa5518b831d57b08608de62d4
zeroequaltwo <youngeunkim373@gmail.com>
2024-11-13 21:33:55 +0900

    feat: [PA-18] write sb doc of Select component

---
+++ src/stories/Select.stories.tsx
@@ -0,0 +1,42 @@
+import { Meta, StoryFn } from '@storybook/react';
+
+import { Select, Props as SelectProps } from '@/components/form/Select';
+import { options } from '@/stories/constants';
+
+export default {
+  component: Select,
+  title: 'Components/Form/Select',
+  argTypes: {
+    options: { control: false },
+    initialOptionId: { control: false },
+    onChange: { control: false },
+  },
+} as Meta<typeof Select>;
+
+const Template: StoryFn<SelectProps> = (args) => {
+  return (
+    <div className={'w-screen h-screen flex flex-col justify-between items-center ml-[-16px] mt-[-16px]'}>
+      <div className={'pt-8'}>
+        <Select {...args} options={options} />
+        <p>{'브라우저에 하단에 남은 공간 > Dropdown의 높이'}</p>
+        <p>Dropdown이 아래로 펼쳐짐</p>
+      </div>
+
+      <div className={'pb-8'}>
+        <Select {...args} options={options} />
+        <p>{'브라우저에 하단에 남은 공간 <= Dropdown의 높이'}</p>
+        <p>Dropdown이 위로 펼쳐짐</p>
+      </div>
+    </div>
+  );
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+  size: 'default',
+  width: 200,
+  state: 'normal',
+  placeholder: '국가를 선택해주세요',
+  allowClear: true,
+};
