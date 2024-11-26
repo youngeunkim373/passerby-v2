110fd777fd48629445a448eb27c6432cbabdd4b2
zeroequaltwo <youngeunkim373@gmail.com>
2024-11-26 22:18:59 +0900

    fix: [PA-43] fix  a build error

--- src/constants/post.ts
+++ src/constants/post.ts
@@ -1,11 +1,21 @@
 export enum Category {
-  JOB = '직장/일',
-  LOVE = '연애',
-  MARRIAGE = '결혼',
-  FAMILY = '가족',
-  PARENTING = '임신/육아',
-  RELATIONSHIP = '인간관계',
-  SCHOOL = '학교생활',
-  CULTURE = '문화생활',
-  ETC = '시시콜콜',
+  JOB = 'JOB',
+  LOVE = 'LOVE',
+  FAMILY = 'FAMILY',
+  PARENTING = 'PARENTING',
+  RELATIONSHIP = 'RELATIONSHIP',
+  SCHOOL = 'SCHOOL',
+  CULTURE = 'CULTURE',
+  ETC = 'ETC',
+};
+
+export const CategoryLabelRecord: Record<Category, string> ={
+  JOB: '직장/일',
+  LOVE: '연애',
+  FAMILY: '가족',
+  PARENTING: '임신/육아',
+  RELATIONSHIP: '인간관계',
+  SCHOOL: '학교생활',
+  CULTURE: '문화생활',
+  ETC: '시시콜콜',
 };
