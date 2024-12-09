682bfb9740f8ec8412e7ae06f0660d5adc969660
zeroequaltwo <youngeunkim373@gmail.com>
2024-12-09 21:52:52 +0900

    refact: [PA-62] improve rendering of the post detail with image loading

--- src/components/common/editor/Viewer.tsx
+++ src/components/common/editor/Viewer.tsx
@@ -1,6 +1,8 @@
 import { Viewer as ToastViewer, ViewerProps } from '@toast-ui/react-editor';
 import dynamic from 'next/dynamic';
-import { ForwardedRef, forwardRef, useRef } from 'react';
+import { ForwardedRef, forwardRef, useRef, useState } from 'react';
+
+import { SpinLoading } from '@/assets/icons/Loading';
 
 /* -------------------- ForwardedViewer -------------------- */
 const WrappedViewewr = dynamic(() => import('./WrappedViewer'), { ssr: false });
@@ -16,6 +18,57 @@ ForwardedViewer.displayName = 'ForwardedViewer';
 
 /* -------------------- Viewer -------------------- */
 export const Viewer = (props: ViewerProps) => {
-  const ref = useRef<ToastViewer>(null);
-  return <ForwardedViewer ref={ref} {...props} />;
+  const viewerRef = useRef<ToastViewer>(null);
+
+  const [ isLoading, setIsLoading ] = useState(true);
+
+  const extractImageElements = (element: HTMLElement): HTMLImageElement[] => {
+    const images = element.querySelectorAll('img');
+    return Array.from(images);
+  };
+
+  const handleLoad = () => {
+    if(!viewerRef.current) return;
+
+    const viewerElement = viewerRef.current.getRootElement();
+    const images = extractImageElements(viewerElement);
+
+    if (images.length === 0) {
+      setIsLoading(false);
+      return;
+    }
+
+    const imageLoadStates = Array.from(images).map((img) => {
+      return new Promise<void>((resolve) => {
+
+        if (img.complete) {
+          resolve();
+        } else {
+          img.addEventListener('load', () => resolve());
+          img.addEventListener('error', () => resolve());
+        }
+      });
+    });
+
+    Promise.all(imageLoadStates)
+      .then(() => setIsLoading(false));
+  };
+
+  return (
+    <>
+      {isLoading && (
+        <div className={'w-full h-[300px] flex flex-col justify-center items-center gap-2'}>
+          <SpinLoading className={'!size-12'} />
+          <span className={'font-semibold text-gray-500'}>Loading..</span>
+        </div>
+      )}
+
+      <ForwardedViewer 
+        ref={viewerRef} 
+        className={isLoading ? 'invisible' : 'visible'}
+        onLoad={handleLoad}
+        {...props} />
+    </>
+
+  );
 };
