28f5ebdd9510283f5b622ac61ae22225f53d7242
zeroequaltwo <youngeunkim373@gmail.com>
2024-12-25 01:32:39 +0900

    feat: [PA-80] create search section for home

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,14 @@ const config: Config = {
     },
   ],
   theme: {
+    screens: {
+      'xs': '420px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     colors: {
       background: 'var(--background)',
       foreground: 'var(--foreground)',
