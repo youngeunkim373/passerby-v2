90a17f4b279035a60e2b3b73fc8f042631ba12f2
0=2 dev <104338021+youngeunkim373@users.noreply.github.com>
2024-12-26 00:52:00 +0900

    feat: [PA-81] improve to allow searching by date when viewing posts (#73)

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -19,3 +19,12 @@ export const getTimeAgo = (timestamp: number) => {
 
   return '방금 전';
 };
+
+export const formatDate = (date: Date) => {
+  const yyyy = date.getFullYear();
+  const mm = String(date.getMonth() + 1).padStart(2, '0');
+  const dd = String(date.getDate()).padStart(2, '0');
+  
+  return `${yyyy}-${mm}-${dd}`;
+};
+
