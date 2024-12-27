de29d92322d4144a00f9639987d5d777c1bbc701
0=2 dev <104338021+youngeunkim373@users.noreply.github.com>
2024-12-27 15:05:03 +0900

    fix: [PA-96] fix signup form State bug (#81)
    * fix: [PA-96] fix signup form State bug
    
    * trivial: [PA-96] replace watched values with normal values inside functions
    
    * trivial: [PA-96] disable nickname Field during signup

--- src/app/join/join.interface.ts
+++ src/app/join/join.interface.ts
@@ -3,7 +3,8 @@ import { Ages, Regions, Sexes } from '@/app/_data/users.interface';
 export interface JoinRequestDTO {
   email: string;
   password: string;
-  nickname: string;
+  // TODO 마이페이지 기능 개발할 때 활성화
+  // nickname: string;
   age: Ages;
   sex: Sexes;
   region: Regions;
